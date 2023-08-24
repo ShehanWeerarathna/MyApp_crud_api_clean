e4212f15cadd6e8d6c10dc6e2158313d754c344e
Chamith Shehan Weerarathna <66713045+shehanweerarathne@users.noreply.github.com>
2023-08-24 11:20:55 +0530

    Initial commit

---
+++ MyApp.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.Domain.Entities;
+
+namespace MyApp.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .HasOne<Category>(s => s.Category)
+            .WithMany(x => x.Products)
+            .HasForeignKey(s => s.CategoryId);
+        // modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        // base.OnModelCreating(modelBuilder);
+    }
+}
